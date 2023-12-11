5143a94fe3463264f355af0222569fa4169a5f2c
MakualDev <31779213+Makual@users.noreply.github.com>
2023-12-11 03:58:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,14 +8,14 @@ import plotly.express as px
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
-# Load Data
+
 @st.cache_data
 def load_data(file_path):
     return pd.read_csv(file_path)
 
 df = load_data('CalomirisPritchett_data.csv')
 
-# Data Preprocessing Functions
+
 def preprocess_data(df):
     df = df[['Sales Date', 'Sex', 'Age', 'Color', 'Price']]
     df = df.rename(columns=lambda x: x.strip())
@@ -32,11 +32,11 @@ def preprocess_data(df):
 
 df = preprocess_data(df)
 
-# Streamlit Application
+
 def create_app(df):
     st.title("New Orlean's Slave Sales Analysis")
 
-    # Add data description from the first code
+
     st.markdown("### Data Description")
     st.text("Sales Date - The date of the transaction (MM/DD/YYYY format). This is NOT the date the sale was recorded in the conveyance office.")
     st.text("Sex Male (M) or female (F). - Gender often inferred from sale record.")
@@ -48,45 +48,45 @@ def create_app(df):
     st.markdown("### Data Overview")
     st.write(df.describe())
 
-    # Date Analysis
+
     df['Year'] = df['Date'].dt.year
     yearly_counts = df['Year'].value_counts().sort_index()
-    # Filtered data from 1856-1865
+
     yearly_counts = yearly_counts[(yearly_counts.index >= 1856) & (yearly_counts.index <= 1865)]
-    # Plotly Bar Chart for Number of Records per Year
+
     fig_years = px.bar(yearly_counts, x=yearly_counts.index, y=yearly_counts.values, title="Number of Records per Year")
     fig_years.update_layout(xaxis_title="Year", yaxis_title="Number of Records")
     st.plotly_chart(fig_years)
 
-    # Price Analysis
+
     st.markdown("### Price Analysis")
     st.write(df['Price'].describe())
     st.write(f"Median price is: {df['Price'].median()}")
 
-    # Plotly Histogram for Price Distribution
+
     fig_price = px.histogram(df[df['Price'] < 5000], x='Price', nbins=10, title="Price Distribution")
     fig_price.update_layout(xaxis_title="Price", yaxis_title="Frequency")
     st.plotly_chart(fig_price)
 
     st.markdown("Most slaves are sold for a price around the median of 1200.")
 
-    # Age Distribution
+
     st.markdown("### Age Distribution")
     st.write(df['Age'].describe())
     st.write(f"Median age is: {df['Age'].median()}")
 
-    # Plotly Histogram for Age Distribution
+
     fig_age = px.histogram(df, x='Age', nbins=10, title="Age Distribution")
     fig_age.update_layout(xaxis_title="Age", yaxis_title="Frequency")
     st.plotly_chart(fig_age)
 
     st.markdown("Age 20 is the most common. The mean age is 26, standard deviation is 11.")
 
-    # Detailed Overview (Annual Statistics)
+
     cut_df = df[(df['Year'] >= 1856) & (df['Year'] <= 1861)]
     annual_stat = {year: cut_df[cut_df['Year'] == year].describe() for year in range(1856, 1862)}
 
-    # Plotly Bar Chart for Annual Mean Price
+
     fig_annual_price = px.bar(
         x=range(1856, 1862),
         y=[annual_stat[i]['Price']['mean'] for i in annual_stat],
@@ -95,7 +95,7 @@ def create_app(df):
     fig_annual_price.update_layout(xaxis_title="Year", yaxis_title="Price")
     st.plotly_chart(fig_annual_price)
 
-    # Plotly Bar Chart for Annual Mean Age
+
     fig_annual_age = px.bar(
         x=range(1856, 1862),
         y=[annual_stat[i]['Age']['mean'] for i in annual_stat],
@@ -110,12 +110,12 @@ def create_app(df):
     percentage_threshold = 1
     color_counts = color_counts[color_counts / total_count * 100 >= percentage_threshold]
 
-    # Plotly Pie Chart for Skin Color Distribution
+
     fig_color = px.pie(color_counts, names=color_counts.index, values=color_counts, title='Color Distribution')
     fig_color.update_traces(textinfo='percent+label')
     st.plotly_chart(fig_color)
     
-    # Skin Color Statistics
+
     color_stat = {color: df[df['Color'] == color].describe() for color in df['Color'].unique() if not df[df['Color'] == color].empty}
     
     color_stat_df = pd.DataFrame({
@@ -127,7 +127,6 @@ def create_app(df):
     
     st.markdown("Black is the most valuable color category, with mean ages across skin colors ranging from 24-26.")
 
-    # Hypothesis Checking
     st.markdown("### Hypothesis Checking")
     st.markdown('Hypothesis: "Is correlation and distribution between age and price the same for Male and Female?"')
 
@@ -144,7 +143,6 @@ def create_app(df):
     st.write(f'Correlation between Age and Price for Female slaves: {female_corr:.3f}')
     st.write(f'Correlation between Age and Price for Male slaves: {male_corr:.3f}')
 
-    # Matplotlib Visualization for Mean Price of Slaves in Each Age Group
     
     age_group_mean_price_female = female_df[female_df['Price'] < 1500].groupby('Age_Group')['Price'].mean()
     plt.figure(figsize=(10, 6))
@@ -166,7 +164,6 @@ def create_app(df):
     
     st.pyplot(plt)
 
-    # Calculate correlations for different age groups between age and price
     male_df_age_0_20 = male_df[(male_df['Age'] >= 0) & (male_df['Age'] <= 20)]
     male_df_age_20_80 = male_df[(male_df['Age'] > 20) & (male_df['Age'] <= 80)]
 
